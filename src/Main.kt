e45e5f661c50b348aac7a18a107723543fa01b00
guilherme-braga4 <bragaguilherme152@yahoo.com.br>
2024-07-04 11:47:47 -0300

    feat: variables

--- src/Main.kt
+++ src/Main.kt
@@ -2,4 +2,13 @@ fun main() {
     println("What's your name?")
     val name = readln()
     println("Hello, $name!")
+
+    val popcorn = 5    // There are 5 boxes of popcorn
+    val hotdog = 7     // There are 7 hotdogs
+    var customers = 10 // There are 10 customers in the queue
+
+    customers = 8
+    println(customers)
+
+    // hotdog = 2 - Val its readonly
 }
